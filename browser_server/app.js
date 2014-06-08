ba4498ef6c6da6ceb38893a821c4fd5ace7e993e
DHCP Server <hello@incrediblemachines.net>
2014-06-08 19:49:12 -0400

    I lied, setting back to 3000

--- browser_server/app.js
+++ browser_server/app.js
@@ -13,7 +13,7 @@ var app = express();
 
 var settings=require( __dirname +'/routes/settings');
 
-app.set('port', process.env.PORT || 80);
+app.set('port', process.env.PORT || 3000);
 app.set('title', 'Camera Controller');
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
@@ -36,7 +36,3 @@ app.post('/cameras/save', settings.saveCamera(Database))
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port').toString());
 });
-
-
-
-
