44d4186971f1757fe1478165d7e71f18b578025f
jmsaavedra <mailbox.jms@gmail.com>
2014-06-08 17:43:05 -0400

    updated capture.js to use the onoff npm and updated package.json to match

--- pi_code/capture/capture.js
+++ pi_code/capture/capture.js
@@ -1,94 +1,63 @@
-//WEBSOCKET URL
+/* CAPTURE.JS
 
-var websocket =''
-var browserapp = ''
+  - receives 'go' from trigger server
+  - toggles SHUTTER and AF gpio pins
+  - sends cameraPost to server after photo is taken
+*/
 
-var gpio = require('pi-gpio');
+var Gpio = require('onoff').Gpio;
 var WebSocket = require('ws')
 var os = require('os')
 var http = require('http')
-
 var fs = require('fs')
+var exec = require('child_process').exec
+var ws = new WebSocket('ws://169.254.233.80:1234');
 
-var ws = new WebSocket('ws://169.254.225.128:1234');
-//var exec = require('child_process').exec
-var spawn = require('child_process').spawn
 
-var gpioOpen = false
-
-var PIN1 = 7
-var PIN2 = 11
-
-var networkInterfaces = os.networkInterfaces()
-console.log(networkInterfaces)
-
+//** VAR DECLARATIONS
+var PIN = [4, 17]; // [SHUTTER, AF]
 var serialNumber = ''
 var ipAddress = '';
-if(networkInterfaces.hasOwnProperty('eth0')){
-  ipAddress = networkInterfaces.eth0[0].address
-}else{
-  ipAddress = 'undefined'
+var websocket =''
+var browserapp = ''
+
+//** EXPORT GPIO PINS
+for(var i=0; i<PIN.length; i++){
+  var strExec = 'gpio export '+PIN[i]+ ' out';
+  var child = exec(strExec,function(error,stdout,stderr){
+    if(error) console.log("GPIO ERROR: "+error);
+  })
 }
 
+//*** SETUP PINS
+var PIN_SHUTTER   = new Gpio(PIN[0], 'out');
+var PIN_AF        = new Gpio(PIN[1], 'out');
+
+//*** SETUP NETWORK CONNECTION
+var networkInterfaces = os.networkInterfaces();
+console.log(networkInterfaces);
+
+if(networkInterfaces.hasOwnProperty('eth0')){
+  ipAddress = networkInterfaces.eth0[0].address;
+}else ipAddress = 'undefined';
+
+
+//*** OPEN WEBSOCKET
 ws.on('open', function() {
   http.get("http://localhost:8080/get?eosserialnumber", function(res) {
     res.on('data', function (chunk) {
       serialnumber = JSON.parse(chunk.toString())['eosserialnumber']
       console.log('Camera Serial: '+serialnumber)
       //console.log('BODY: ' + chunk);
-
       var obj = {'address':ipAddress, 'serial':serialnumber}
       console.log(obj)
       var objstring = JSON.stringify(obj)
       ws.send(objstring);
       newCameraPost(objstring)
-
     });
   }).on('error', function(e) {
     console.log("Got error: " + e.message);
   });
-
-});
-
-gpio.open(PIN1, "output",function(err){
-  console.log('GPIO pin '+PIN1+' open')
-  writeLow(PIN1)
-  gpioOpen = true
-});
-gpio.open(PIN2, "output",function(err){
-  console.log('GPIO pin '+PIN2+' open')
-  writeLow(PIN2)
-  gpioOpen = true
-});
-
-console.log("Running GPIO")
-
-var state = true
-
-
-ws.on('message', function(data, flags) {
-  if(data == 'go'){
-    console.log(data)
-    // flags.binary will be set if a binary data is received
-    // flags.masked will be set if the data was masked
-
-    // var child = exec('gphoto2 --capture-image-and-download',function(error,stdout,stderr){
-    //   console.log('stdout: '+stdout)
-    //   console.log('stderr: '+stderr)
-    // })
-    if(gpioOpen == true){
-      writeHigh(PIN1)
-      setTimeout(function(){
-         state=false
-         writeLow(PIN1)
-       },300)
-    }
-  }else if(data == 'close'){
-    var child = exec('echo raspberry | sudo shutdown -h now',function(error,stdout,stderr){
-      console.log('stdout: '+stdout)
-      console.log('stderr: '+stderr)
-    })
-  }
 });
 
 ws.on('close',function(){
@@ -96,50 +65,71 @@ ws.on('close',function(){
   console.log('disconnected')
 })
 
-function writeHigh(pin) {
-    gpio.write(pin, 1, function() {
-        console.log(pin+' pin high');
-    });
-    // gpio.write(PIN2,0,function(){
-    //     console.log(PIN2+' pin low')
-    // })
+
+//*** HANDLE WEBSOCKET MESSAGES
+ws.on('message', function(data, flags) {
+  if(data == 'go'){
+    console.log(data)
+    // flags.binary will be set if a binary data is received
+    // flags.masked will be set if the data was masked
+    hitShutter(); //*** TAKE PICTURE !!
+  }
+  else if(data == 'close'){
+    var child = exec('echo raspberry | sudo shutdown -h now',function(error,stdout,stderr){
+      console.log('stdout: '+stdout)
+      console.log('stderr: '+stderr)
+    })
+  }
+});
+
+//** TAKE A PICTURE !! **
+function hitShutter(){
+  digitalWrite(PIN_SHUTTER, 1);
+  setTimeout(function(){
+     digitalWrite(PIN_SHUTTER, 0);
+   },300); //press duration
 }
-function writeLow(pin) {
-    gpio.write(pin, 0, function() {
-        console.log(pin+' pin low');
-    });
-    // gpio.write(PIN2, 1, function() {
-    //     console.log(PIN2+' pin high');
-    // });
+
+//** half-press for Auto-Focus
+function hitAutoFocus(){
+  digitalWrite(PIN_AF, 1);
+  setTimeout(function(){
+     digitalWrite(PIN_AF, 0);
+  },300); //press duration
 }
+
+//** digitalWrite function
+function digitalWrite(pin, state){
+  pin.write(state, function(err) { // Asynchronous write.
+    //pin.write(value === 0 ? 1 : 0, function(err) {
+      if (err) throw err;
+  });
+}
+
 
 function newCameraPost(objstring){
-  var options = {
-    host:browserapp,
-    port:80,
-    path:'/camera',
-    method:'POST',
-    headers:{
-    'Content-Type': 'application/json',
-    'Content-Length': objstring.length
-    }
-  }
-  // also send to browser computer
-  var req = http.request(options,function(res){
-
-  })
-  req.write(objstring)
-  req.end()
+  // var options = {
+  //   host:browserapp,
+  //   port:80,
+  //   path:'/camera',
+  //   method:'POST',
+  //   headers:{
+  //   'Content-Type': 'application/json',
+  //   'Content-Length': objstring.length
+  //   }
+  // }
+  // // also send to browser computer
+  // var req = http.request(options,function(res){
+  //
+  // })
+  // req.write(objstring)
+  // req.end()
 }
 
+
+
 process.on('SIGINT', function() {
-
   console.log(' Got SIGINT closing gpio')
-
-  gpio.close(PIN1);
-  gpio.close(PIN2);
-
   console.log(' Goodbye ')
   process.exit(0)
-
 })
