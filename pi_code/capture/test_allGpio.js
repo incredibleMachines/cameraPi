a7d20c3d34b706aef5c726d4fd4730193f5c240a
jmsaavedra <mailbox.jms@gmail.com>
2014-06-08 20:02:33 -0400

    updating RC2 with all files

--- pi_code/capture/test_allGpio.js
+++ pi_code/capture/test_allGpio.js
@@ -8,17 +8,32 @@
   - small LED OUTPUT pin
   - Laser INPUT pin
 
-  - NO WEBSOCKET CONNECTIONS. ALL COMMENTED OUT //************
+
+  //** WEBSOCKET CONNECTIONS DISABLED //************
+
 */
+
+//*******************
+
+var ENABLE_WS = true; //ENABLE_WEBSOCKETS ?
+
+//* TRUE HOST_IP: "192.168.0.3"
+var HOST_IP = "192.168.0.42"
+var PORT    = "1234"
+
+//*******************
 
 
 var Gpio = require('onoff').Gpio;
-// var WebSocket = require('ws') //************
 var os = require('os')
 var fs = require('fs')
+var http = require('http')
 var exec = require('child_process').exec
-// var ws = new WebSocket('ws://169.254.233.80:1234');//***********
 
+if(ENABLE_WS){
+  var WebSocket = require('ws') //************
+  var ws = new WebSocket('ws://'+HOST_IP+':'+PORT);//***********
+}
 //all should be configured to OUT except for pin 23.
 var PIN = [23, 4, 17, 24, 27, 25];
 
@@ -55,53 +70,66 @@ setTimeout(function(){
  },1000); //wait a second for Gpio(pin) to config first
 
 
+if(ENABLE_WS){
+  var networkInterfaces = os.networkInterfaces() //*************
+  console.log(networkInterfaces)
 
-// var networkInterfaces = os.networkInterfaces() //*************
-// console.log(networkInterfaces)
-//
-// var ipAddress = '';
-// if(networkInterfaces.hasOwnProperty('eth0')){
-//   ipAddress = networkInterfaces.eth0[0].address
-// }else{
-//   ipAddress = 'undefined'
-// }
-
-//** OPEN WEBSOCKET **
-// ws.on('open', function() {  //************************
-//   var obj = {'address':ipAddress}
-//   console.log(obj)
-//   ws.send(JSON.stringify(obj));
-// });
-//
-//
-// //** WEBSOCKET MESSAGE HANDLING **
-// ws.on('message', function(data, flags) {
-//   if(data == 'go'){
-//     console.log(data)
-//     // flags.binary will be set if a binary data is received
-//     // flags.masked will be set if the data was masked
-//     hitShutter();
-//   }
-//
-//   else if(data == 'close'){
-//     var child = exec('echo raspberry | sudo shutdown -h now',function(error,stdout,stderr){
-//       console.log('stdout: '+stdout)
-//       console.log('stderr: '+stderr)
-//     })
-//   }
-//
-//   else if(data == 'toggleleader'){
-//     configLaser(!groupLeader);
-//   }
-// });
+  var ipAddress = '';
+  if(networkInterfaces.hasOwnProperty('eth0')){
+    ipAddress = networkInterfaces.eth0[0].address
+  }else{
+    ipAddress = 'undefined'
+  }
 
 
+  //*** OPEN WEBSOCKET
+  ws.on('open', function() {
+    http.get("http://localhost:8080/get?eosserialnumber", function(res) {
+      res.on('data', function (chunk) {
+        serialnumber = JSON.parse(chunk.toString())['eosserialnumber']
+        console.log('Camera Serial: '+serialnumber)
+        //console.log('BODY: ' + chunk);
+        var obj = {'address':ipAddress, 'serial':serialnumber}
+        console.log(obj)
+        var objstring = JSON.stringify(obj)
+        ws.send(objstring);
+        newCameraPost(objstring)
+      });
+    }).on('error', function(e) {
+      console.log("Got error: " + e.message);
+    });
+  });
+
+
+  //** WEBSOCKET MESSAGE HANDLING **
+  ws.on('message', function(data, flags) {
+    if(data == 'go'){
+      console.log(data)
+      // flags.binary will be set if a binary data is received
+      // flags.masked will be set if the data was masked
+      hitShutter();
+    }
+
+    else if(data == 'close'){
+      var child = exec('echo raspberry | sudo shutdown -h now',function(error,stdout,stderr){
+        console.log('stdout: '+stdout)
+        console.log('stderr: '+stderr)
+      })
+    }
+
+    else if(data == 'toggleleader'){
+      configLaser(!groupLeader);
+    }
+  });
+} //END IF (ENABLE_WS)
+
+
+//**** setup or disable laser pin
 function configLaser(job){
   groupLeader = job;
   digitalWrite(standbyLed, 0);
 
-  if(groupLeader){ //watch sensor!
-
+  if(groupLeader){ //enable sensor!
     //** set watch with callback on laserpin RISING
     PIN_LASER.watch(function(err, value) {
         //console.log("LASER TRIP RISING DETECTED");
@@ -112,7 +140,7 @@ function configLaser(job){
     standbyLed = PIN_LED_GRN;
   }
 
-  else{
+  else{ //disable sensor
     PIN_LASER.unwatch(function(err){
       if (err) throw err;
     })
@@ -131,7 +159,9 @@ function laserTriggered(){
     digitalWrite(PIN_LED_RED, 1);
     digitalWrite(standbyLed, 0);
 
-    hitShutter(); //--- for now, toggle shutter pin. DEBUG only
+    //if(groupLeader) hitShutter(); //--- for now, toggle shutter pin. DEBUG only
+
+    //SEND TRIGGER TO SERVER HERE
 
     setTimeout(function(){
        triggerState = false; //reset trigger
