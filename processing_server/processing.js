a9cb4ba5ad7a72f5823fd84444980d9bdf0168f6
Download Server <controlfreak@CFS-Download.local>
2014-06-12 14:32:15 -0400

    Initial commit of processing_server Working file naming, warping, cropping, variable framerate encoding, and concat Creates four folders - warped, cropped, movs, output Output stores a file with takename for final distribution

---
+++ processing_server/processing.js
@@ -0,0 +1,42 @@
+var express = require('express')
+var http = require('http')
+var path = require('path')
+var fs = require('fs')
+var http = require('http')
+
+var processing=require( __dirname +'/routes/processing');
+
+var app = express()
+
+
+
+app.set('port', process.env.PORT || 3002)
+app.set('title', 'Image Downloader')
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+app.set("jsonp callback", true)
+app.use(express.favicon())
+app.use(express.logger('dev'))
+app.use(express.bodyParser({limit:'1000mb', uploadDir: 'tmp', keepExtensions: true})) //temporary folder to store images on upload
+app.use(express.methodOverride())
+app.use(express.cookieParser('30_PP_ControllerApp'))
+app.use(express.session({secret: '!@#$%^&*()1234567890qwerty'}))
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(app.router)
+
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler())
+}
+
+var imageCounter = 0;
+
+
+app.get('/',processing.cropImages())
+// app.get('/render',processing.renderVideo())
+
+
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port '+ app.get('port').toString())
+});
