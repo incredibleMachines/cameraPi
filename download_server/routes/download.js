4500100a4e37c6454c5b2cb08a337879f1a7c0ae
controlfreak <controlfreak@CFS-Download.local>
2014-06-12 02:50:42 -0400

    Fixed download folder generation :wq

--- download_server/routes/download.js
+++ download_server/routes/download.js
@@ -1,7 +1,38 @@
-var fs = require('fs'),
+var fs = require('fs')
 var http=require('http')
 
-exports.saveImage()= function(MongoDB){
+var BROWSER_IP = 'http://192.168.0.4'
+
+var deviceList = []
+var currentDirectory
+
+
+getCameras(function(_deviceList){
+    deviceList = _deviceList
+})
+
+exports.getCameraInfo = function (){
+return function(req,res){
+    getCameras(function(_deviceList){
+      deviceList = _deviceList
+      res.jsonp(_deviceList)
+    })
+}
+}
+
+exports.initDownload= function(){
+  return function(req,res){
+  var time=new Date().getTime()
+  fs.mkdir('images/'+time,function(){
+    currentDirectory='images/'+time
+  })
+}
+}
+
+exports.saveImage = function(){
+  return function (req,res) {
+  console.log("hit")
+
   res.jsonp({post:true})
   //console.log(req)
   var post = req.body;
@@ -12,18 +43,37 @@ exports.saveImage()= function(MongoDB){
   //console.log(image)
   var file = image.originalFilename.substr(0,image.originalFilename.indexOf('.'))
   //need to create unique image paths here
-  
-  http.get('http://192.168.0.4:3000/getinfo', function(server){
-		console.log(server)
-	}).on('error', function(e) {
-		console.log("Got error: " + e.message)
-	})
-  
-  var time=new Date().getTime()
-  fs.mkdir('images/'+)
-  fs.rename(image.path,'images/'+req.connection.remoteAddress+'.jpg', function(err){
+
+  var camera_id
+  deviceList.forEach(function(camera,i){
+      if(camera['address']==req.connection.remoteAddress){
+        console.log('Camera'+camera['camera_id'])
+        camera_id=camera['camera_id']
+      }
+  })
+  fs.rename(image.path,'images/'+currentDirectory+'/'+camera_id+'.jpg', function(err){
     imageCounter++;
     if(err) console.error(err)
     console.log('Image Saved.')
   })
+}
+}
+
+function getCameras(cb){
+  var info = ''
+  http.get(BROWSER_IP+':3000/getinfo',function(res){
+    res.on('data', function (chunk) {
+        info+=chunk
+    })
+    res.on('close',complete)
+    res.on('end',complete)
+  }).on('error',function(e){
+    console.error(e)
+  })
+
+  function complete(){
+    cb(JSON.parse(info))
+    //res.jsonp(deviceList)
+  }
+
 }
