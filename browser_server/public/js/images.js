63ab3473349ad3723b90af7176acebebbeec695f
DHCP Server <hello@incrediblemachines.net>
2014-06-13 15:21:29 -0400

    Working calibration settings saving, checking public folder in downloads for calibration images

--- browser_server/public/js/images.js
+++ browser_server/public/js/images.js
@@ -1,21 +1,23 @@
-var editPoints 	
-var currentCamera
+var editPoints
+var currentCamera='001'
+
+var DOWNLOAD_IP='192.168.0.2'
 
 var loadPoints = function(vars,cur){
-	currentCamera=parseInt(cur)
-	console.log("CURRENT:"+currentCamera)	
-	editPoints=vars[currentCamera].warp
-	
+	currentCamera=cur
+	console.log("CURRENT:"+currentCamera)
+	editPoints=vars[parseInt(currentCamera)].warp
+
 }
 
 $(document).ready(function(){
 	var method="psr"
-	
+
 	var mouseThreshold=20
-	
+
 	var imgPoints = [{x:0,y:0},{x:960,y:0},{x:960,y:640},{x:0,y:640} ]
 /* 	var editPoints =  */
-	
+
 		$('#0X').attr("value",editPoints[0]["x"])
 		$('#0Y').attr("value",editPoints[0]["y"])
 		$('#1X').attr("value",editPoints[1]["x"])
@@ -24,15 +26,15 @@ $(document).ready(function(){
 		$('#2Y').attr("value",editPoints[2]["y"])
 		$('#3X').attr("value",editPoints[3]["x"])
 		$('#3Y').attr("value",editPoints[3]["y"])
-	
+
 	var c=document.getElementById("image-editor")
 	var ctx=c.getContext("2d")
-	var img=new Image()	
-	
+	var img=new Image()
+
 	var selected=null
-	
-	
-	
+
+
+
 	$('#psr').click(function(){
 		method="psr"
 		console.log(method)
@@ -43,8 +45,8 @@ $(document).ready(function(){
 		console.log(method)
 		$('#psr').prop('checked',false);
 	})
-	
-	
+
+
 		img.onload = function(){
 
 			drawImage()
@@ -52,9 +54,9 @@ $(document).ready(function(){
 
 
 		}
-		
-		img.src = "test_images/test_image.jpg"
-	
+
+		img.src = "http://"+DOWNLOAD_IP+":3001/calibration/"+currentCamera+".jpg"
+
 		$(document).click(function(event){
 		if(method=="quad"){
 			var x=event.clientX-c.offsetLeft+$(window).scrollLeft()
@@ -69,7 +71,7 @@ $(document).ready(function(){
 						clicked=true
 					}
 				}
-			
+
 			}
 			if(clicked==false){
 				selected=null
@@ -81,7 +83,7 @@ $(document).ready(function(){
 		$(document).keydown(function(event){
 			console.log(editPoints)
 
-			
+
 			if(event.shiftKey==true){
 				moveModifier=5
 			}
@@ -91,8 +93,8 @@ $(document).ready(function(){
 			else{
 				moveModifier=1
 			}
-		
-			if(event.which==38){	
+
+			if(event.which==38){
 				event.preventDefault()
 				if(method=="psr"){
 					for(var i=0; i<editPoints.length;i++){
@@ -116,7 +118,7 @@ $(document).ready(function(){
 					adjustPosition(moveModifier,0,selected)
 				}
 			}
-			else if(event.which==40){	
+			else if(event.which==40){
 				event.preventDefault()
 				if(method=="psr"){
 					for(var i=0; i<editPoints.length;i++){
@@ -128,7 +130,7 @@ $(document).ready(function(){
 					adjustPosition(0,moveModifier,selected)
 				}
 			}
-			else if(event.which==37){	
+			else if(event.which==37){
 				event.preventDefault()
 				if(method=="psr"){
 					for(var i=0; i<editPoints.length;i++){
@@ -146,14 +148,14 @@ $(document).ready(function(){
 			else if(event.which==65){
 				adjustZoom(-moveModifier)
 			}
-			
+
 			else if(event.which==188){
 				adjustRotate(-moveModifier)
 			}
 			else if(event.which==190){
 				adjustRotate(moveModifier)
 			}
-			
+
 			drawImage()
 			$('#0X').attr("value",editPoints[0]["x"])
 			$('#0Y').attr("value",editPoints[0]["y"])
@@ -165,14 +167,14 @@ $(document).ready(function(){
 			$('#3Y').attr("value",editPoints[3]["y"])
 		})
 
-	
+
 	function drawImage(){
 			ctx.clearRect(0,0,c.width, c.height)
 			ctx.drawImage(img,imgPoints[0]["x"],imgPoints[0]["y"],imgPoints[2]["x"],imgPoints[2]["y"])
 
 			ctx.beginPath()
 			ctx.moveTo(editPoints[0]["x"],editPoints[0]["y"])
-			ctx.lineTo(editPoints[1]["x"],editPoints[1]["y"])		
+			ctx.lineTo(editPoints[1]["x"],editPoints[1]["y"])
 			ctx.lineTo(editPoints[2]["x"],editPoints[2]["y"])
 			ctx.lineTo(editPoints[3]["x"],editPoints[3]["y"])
 			ctx.lineTo(editPoints[0]["x"],editPoints[0]["y"])
@@ -190,7 +192,7 @@ $(document).ready(function(){
 				ctx.fillStyle='#00ff00'
 			}
 			ctx.fill()
-			
+
 			ctx.beginPath()
 			ctx.arc(editPoints[1]["x"],editPoints[1]["y"], 3, 0, 2 * Math.PI, false)
 			ctx.closePath()
@@ -199,7 +201,7 @@ $(document).ready(function(){
 				ctx.fillStyle='#00ff00'
 			}
 			ctx.fill()
-			
+
 			ctx.beginPath()
 			ctx.arc(editPoints[2]["x"],editPoints[2]["y"], 3, 0, 2 * Math.PI, false)
 			ctx.closePath()
@@ -208,7 +210,7 @@ $(document).ready(function(){
 				ctx.fillStyle='#00ff00'
 			}
 			ctx.fill()
-			
+
 			ctx.beginPath()
 			ctx.arc(editPoints[3]["x"],editPoints[3]["y"], 3, 0, 2 * Math.PI, false)
 			ctx.closePath()
@@ -218,12 +220,12 @@ $(document).ready(function(){
 			}
 			ctx.fill()
 	}
-	
+
 	function adjustPosition (x, y, which){
 		editPoints[which]["x"]=parseFloat(editPoints[which]["x"])+x
 		editPoints[which]["y"]=parseFloat(editPoints[which]["y"])+y
-	}	
-	
+	}
+
 	function adjustZoom (amt){
 		editPoints[0]["x"]=parseFloat(editPoints[0]["x"])-amt
 		editPoints[0]["y"]=parseFloat(editPoints[0]["y"])-amt
@@ -234,10 +236,10 @@ $(document).ready(function(){
 		editPoints[3]["x"]=parseFloat(editPoints[3]["x"])-amt
 		editPoints[3]["y"]=parseFloat(editPoints[3]["y"])+amt
 	}
-	
+
 	function adjustRotate (angle){
 		var arr=[[editPoints[0]["x"],editPoints[0]["y"]],[editPoints[1]["x"],editPoints[1]["y"]],[editPoints[2]["x"],editPoints[2]["y"]],[editPoints[3]["x"],editPoints[3]["y"]]]
-		
+
 		var x = arr.map(function(a){ return a[0] });
 	    var y = arr.map(function(a){ return a[1] });
 	    var minX = Math.min.apply(null, x);
@@ -246,7 +248,7 @@ $(document).ready(function(){
 	    var maxY = Math.max.apply(null, y);
 	    var centerX=(minX + maxX)/2
 	    var centerY= (minY + maxY)/2
-	    
+
 		var rotate=rotate_point(editPoints[0]["x"],editPoints[0]["y"],centerX,centerY,angle)
 		editPoints[0]["x"]=rotate.x
 		editPoints[0]["y"]=rotate.y
@@ -260,7 +262,7 @@ $(document).ready(function(){
 		editPoints[3]["x"]=rotate.x
 		editPoints[3]["y"]=rotate.y
 	}
-	
+
 	function rotate_point(pointX, pointY, originX, originY, angle) {
 	    angle = angle * Math.PI / 180.0;
 	    return {
@@ -268,8 +270,6 @@ $(document).ready(function(){
 	        y: Math.sin(angle) * (pointX-originX) + Math.cos(angle) * (pointY-originY) + originY
 	    };
 	}
-		
+
 
 })
-
-
