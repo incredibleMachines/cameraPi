0e0ee286174ed5629896dcb7cd4f167e472ce562
Download Server <controlfreak@CFS-Download.local>
2014-06-13 04:21:57 -0400

    "changes to downloads and processing apps for capture

--- download_server/routes/download.js
+++ download_server/routes/download.js
@@ -1,13 +1,52 @@
 var fs = require('fs')
 var http=require('http')
+var dgram=require('dgram')
+
 
 var BROWSER_IP = 'http://192.168.0.4'
+var DOWNLOAD_IP = '127.0.0.1'
 
 var deviceList = []
 var currentDirectory
+var currentCamera
 
 var imagecount = 0
 var imagetotal = 20
+
+var timedPhoto
+
+var method="armed"
+
+var live=0
+var recording=false
+
+
+/* UDP Server Functionality */
+var server = dgram.createSocket('udp4')
+server.bind(41234);
+
+
+/* UDP SERVER ERROR */
+server.on('error',function(err){
+  console.log('Server Error: \n'+err.stack)
+  console.log('Warning Server has been closed and must be restarted')
+  server.close()
+})
+
+//** UDP MESSAGE RECEIVED **/
+server.on('message', function (msg, rinfo) {
+  console.log('server got: ' + msg + ' from ' + rinfo.address + ":" + rinfo.port);
+});
+
+/* START LISTENING ON THE SERVER*/
+server.on("listening", function () {
+  server.setBroadcast(true)
+  server.setMulticastTTL(128);
+  //server.addMembership('230.185.192.108');
+  var address = server.address();
+  console.log("server listening " +
+      address.address + ":" + address.port);
+});
 
 
 getCameras(function(_deviceList){
@@ -25,27 +64,70 @@ return function(req,res){
 
 exports.startProcessing=function(){
     return function(req,res){
-      http.get('http://127.0.0.1:3002/?take=1402557537181',function(res){
+      http.get('http://'+DOWNLOAD_IP+':3002/?take=1402557537181',function(res){
           console.log("inited procesing")
       })
+      res.jsonp({"status":"processing"})
     }
 }
 
 exports.initDownload= function(){
   return function(req,res){
-  var time=new Date().getTime()
-  fs.mkdir('images/'+time,function(){
-    currentDirectory='images/'+time
-  })
-  imagecount=0
+    getCameras(function(){
+      console.log("armed")
+        method="armed"
+        var time=new Date().getTime()
+        fs.mkdir('images/'+time,function(){
+          currentDirectory='images/'+time
+          imagecount=0
+          console.log("current: "+currentDirectory)
+          res.jsonp({"status":"download set"})
+
+          })
+
+    })
+
 }
 }
 
 exports.saveImage = function(){
   return function (req,res) {
-  console.log("hit")
 
-  res.jsonp({post:true})
+
+  if(method=="armed"){
+    console.log("hit")
+
+  res.jsonp({post:"armed"})
+  //console.log(req)
+  var post = req.body;
+  //console.log(post)
+  var files = req.files;
+  //console.log(files)
+  var image = files.image
+  // console.log(image)
+  var file = image.originalFilename.substr(0,image.originalFilename.indexOf('.'))
+  //need to create unique image paths here
+console.log(req.connection.remoteAddress)
+
+  var camera_id
+  deviceList.forEach(function(camera,i){
+      if(camera['address']==req.connection.remoteAddress){
+        console.log('Camera: '+camera['camera_id'])
+        camera_id=camera['camera_id']
+        console.log(currentDirectory)
+        fs.rename(__dirname+'/../'+image.path,__dirname+'/../'+currentDirectory+'/'+camera_id+'.jpg', function(err){
+          imagecount++;
+          if(err) console.error(err)
+          console.log('Image Saved.')
+        })
+      }
+  })
+
+
+}
+
+else{
+  res.jsonp({post:"calibration"})
   //console.log(req)
   var post = req.body;
   //console.log(post)
@@ -63,13 +145,60 @@ exports.saveImage = function(){
         camera_id=camera['camera_id']
       }
   })
-  fs.rename(image.path,currentDirectory+'/'+camera_id+'.jpg', function(err){
+  fs.rename(image.path,'/public/'+camera_id+'/+camera_id.jpg', function(err){
     imagecount++;
     if(err) console.error(err)
     console.log('Image Saved.')
   })
+}
 
 }
+}
+
+exports.setMethod=function(){
+  return function(req,res){
+    method=req.param('method')
+  }
+}
+
+exports.captureCalibrationImage = function(){
+  return function(req,res){
+  var message = new Buffer('go')
+  var address
+  console.log(live)
+  deviceList.forEach(function(camera,i){
+      if(camera['camera_id']==currentCamera){
+        address=camera['address']
+      }
+  })
+
+
+if(live==0&&recording==true){
+  clearInterval(timedPhoto)
+  recording=false
+}
+else if(live==0){
+  triggerCamera(message,address)
+}
+else if (recording==false){
+  timedPhoto=setInterval(function() { triggerCamera(message,address) },500)
+  recording=true
+}
+  res.send('sending image trigger');
+}
+}
+
+exports.serveCalibrationImage = function(){
+  return function (req,res){
+    currentCamera=req.param('camera')
+    console.log(req.param('live'))
+    console.log(currentCamera)
+    live=req.param('live')
+    method="calibration"
+
+    var imageURL='/calibration/'+currentCamera+'.jpg'
+    res.redirect('/capture-calibration?camera='+currentCamera)
+  }
 }
 
 function getCameras(cb){
@@ -89,4 +218,9 @@ function getCameras(cb){
     //res.jsonp(deviceList)
   }
 
+}
+
+function triggerCamera(message,address){
+  console.log("photo")
+  server.send(message,0,message.length,41234,address)
 }
--- processing_server/routes/processing.js
+++ processing_server/routes/processing.js
@@ -11,7 +11,9 @@ var deviceList = []
 var currentDirectory
 
 var imagecount = 0
-var imagetotal = 20
+var imagetotal = 1
+
+var totalCams = 30
 
 var width = 4272/960
 var height = 2848/640
@@ -29,30 +31,35 @@ exports.cropImages = function(){
 
   console.log("/Users/controlfreak/Desktop/cameraPi/download_server/images/"+get)
 
+  deleteFolderRecursive("/Users/controlfreak/Desktop/cameraPi/download_server/images/"+get+"/cropped")
+  deleteFolderRecursive("/Users/controlfreak/Desktop/cameraPi/download_server/images/"+get+"/warped")
+  deleteFolderRecursive("/Users/controlfreak/Desktop/cameraPi/download_server/images/"+get+"/movs")
+  deleteFolderRecursive("/Users/controlfreak/Desktop/cameraPi/download_server/images/"+get+"/output")
+
+
   fs.mkdir("/Users/controlfreak/Desktop/cameraPi/download_server/images/"+get+"/cropped",function(){})
-    fs.mkdir("/Users/controlfreak/Desktop/cameraPi/download_server/images/"+get+"/warped",function(){})
-    fs.mkdir("/Users/controlfreak/Desktop/cameraPi/download_server/images/"+get+"/movs",function(){})
-    fs.mkdir("/Users/controlfreak/Desktop/cameraPi/download_server/images/"+get+"/output",function(){})
+  fs.mkdir("/Users/controlfreak/Desktop/cameraPi/download_server/images/"+get+"/warped",function(){})
+  fs.mkdir("/Users/controlfreak/Desktop/cameraPi/download_server/images/"+get+"/movs",function(){})
+  fs.mkdir("/Users/controlfreak/Desktop/cameraPi/download_server/images/"+get+"/output",function(){})
 
     fs.readdir("/Users/controlfreak/Desktop/cameraPi/download_server/images/"+get, function(err, files){
 if(err){
   console.log(err)
 }
 else{
-    console.log("filenumber:"+files)
+    console.log("filenumber: "+files)
 
-    var totalCams = deviceList.length
-    console.log("length:"+totalCams)
+
+    console.log("length: "+totalCams)
     var count=0
-    var length=1
+    var length=10
 
     files.forEach(function (file,i) {
       if(file.indexOf('.jpg')>-1){
         var thisCam=file.slice(0,-4)
         console.log('filename: '+thisCam)
         var currentCamera=_.findWhere(deviceList,{camera_id:thisCam})
-        console.log('filedetails: '+currentCamera.warp[0]["x"])
-
+if(typeof currentCamera!='undefined'){
     var command = "convert /Users/controlfreak/Desktop/cameraPi/download_server/images/"+get+"/"+thisCam+".jpg -matte -virtual-pixel transparent \ -distort Perspective \ '"+currentCamera.warp[0]["x"]*width+","+currentCamera.warp[0]["y"]*height+" 0,0 "+currentCamera.warp[1]["x"]*width+","+currentCamera.warp[1]["y"]*height+" 640,0 "+currentCamera.warp[2]["x"]*width+","+currentCamera.warp[2]["y"]*height+" 640,640 "+currentCamera.warp[3]["x"]*width+","+currentCamera.warp[3]["y"]*height+" 0,640' \ /Users/controlfreak/Desktop/cameraPi/download_server/images/"+get+"/warped/"+thisCam+".jpg"
 
       exec(command,function(error,stdout,stderr){
@@ -65,9 +72,9 @@ else{
           console.log("writing video")
           var inputPath="/Users/controlfreak/Desktop/cameraPi/download_server/images/"+get+"/cropped/"+thisCam+".jpg"
           var outputPath="/Users/controlfreak/Desktop/cameraPi/download_server/images/"+get+"/movs/"+thisCam+".mov"
-          var videocmd = "/usr/local/bin/ffmpeg -f image2 -i "+inputPath+" -r 1:"+length+" "+outputPath
-          length++
+          var videocmd = "/usr/local/bin/ffmpeg -f image2 -i "+inputPath+" -r "+length+" "+outputPath
           exec(videocmd,function(error,stdout,stderr){
+            console.log("video")
             if(!error){
               console.log("video finished!")
               count++
@@ -81,14 +88,17 @@ else{
                     exec(concat,function(error,stdout,stderr){
                       if(!error){
                         console.log("Final Video Rendered! ENJOY!")
+                        res.jsonp({"status":"rendered"})
                       }
                       else{
                         console.log(error)
+                        res.jsonp({"status":"error on concat"})
                       }
                     })
                   }
                   else{
                     console.log(error)
+                    res.jsonp({"status":"error on video"})
                   }
 
                 })
@@ -97,12 +107,14 @@ else{
             }
             else{
               console.log(error)
+              res.jsonp({"status":"error on video"})
             }
           })
         }
         else{
           console.log("gm error!")
           console.log(e)
+          res.jsonp({"status":"error on gm"})
         }
       })
       }
@@ -113,6 +125,7 @@ else{
 
 
       })
+    }
     }
       })
     }
@@ -138,3 +151,19 @@ else{
   }
 
 }
+
+deleteFolderRecursive = function(path) {
+    var files = [];
+    if( fs.existsSync(path) ) {
+        files = fs.readdirSync(path);
+        files.forEach(function(file,index){
+            var curPath = path + "/" + file;
+            if(fs.lstatSync(curPath).isDirectory()) { // recurse
+                deleteFolderRecursive(curPath);
+            } else { // delete file
+                fs.unlinkSync(curPath);
+            }
+        });
+        fs.rmdirSync(path);
+    }
+};
